ef1f393afb8d52af5cb3fd4285885ef2851b5413
Suleiman Sani <suleiman.sani1@gmail.com>
2021-10-27 00:47:49 +0100

    first

--- Program.cs
+++ Program.cs
@@ -8,37 +8,10 @@ namespace Linked_list
     {
         static void Main(string[] args)
         {
-            SLinkedList<int> sule = new SLinkedList<int>();
-            SStack<int> number = new SStack<int>();
-            SQueue<string> num = new SQueue<string>();
-            Console.WriteLine(sule[6]);
-
+            SLinkedList<int> test1 = new SLinkedList<int>();
+            SStack<int> test2 = new SStack<int>();
+            SQueue<string> test3 = new SQueue<string>();
             
-/*
-            num.Enqueue("10");
-            num.Enqueue("9");
-            num.Enqueue("8");
-            num.Enqueue("7");
-            num.Enqueue("6");
-
-            number.Push(10);
-            number.Push(9);
-            number.Push(8);
-            number.Push(7);
-            number.Push(6);
-            number.Push(5);
-
-            sule.Add(34);
-            sule.Add(23);
-            sule.Add(34);
-            Console.WriteLine(sule.del(0));
-            //sule.del("3");
-
-            sule.Print();
-
-
-            */
-
         }
     }
     
